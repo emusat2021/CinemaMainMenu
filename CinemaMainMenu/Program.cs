461180f2b9bc989b824e5a01409ad4aa353d0c3f
Eugenia <musat.eugenia@gmail.com>
2024-02-29 17:06:36 +0100

    Welcome message
    Add functionality for user to exit program
    Add feedback for user if invalid input

--- CinemaMainMenu/Program.cs
+++ CinemaMainMenu/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            bool continueProgram = true;
+            string input = string.Empty;
+
+            do
+            {
+                Console.WriteLine("Welcome to the Cinema's Main Menu!");
+                Console.WriteLine("--------------------------------\n");
+                Console.WriteLine("You have the folowing choices:\n");
+                Console.WriteLine("\"0\" - To Close the program" +
+                    "\n");
+            
+                input = Console.ReadLine();
+
+                switch(input)
+                {
+                    case "0":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+            }while(continueProgram);
+            Console.WriteLine("Closing application");
+
+
         }
     }
 }
