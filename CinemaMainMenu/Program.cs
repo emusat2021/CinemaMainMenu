b369a2042d25bb827c8ec62899853846070cf68e
Eugenia <musat.eugenia@gmail.com>
2024-03-03 11:59:44 +0100

    Add message to user to be able to add user age

--- CinemaMainMenu/Program.cs
+++ CinemaMainMenu/Program.cs
@@ -12,15 +12,33 @@
                 Console.WriteLine("Welcome to the Cinema's Main Menu!");
                 Console.WriteLine("--------------------------------\n");
                 Console.WriteLine("You have the folowing choices:\n");
-                Console.WriteLine("\"0\" - To Close the program" +
-                    "\n");
+                Console.WriteLine("0\t- EXIT the program");
+                Console.WriteLine("1\t- Buy a ticket");
             
                 input = Console.ReadLine();
 
-                switch(input)
+                switch (input)
                 {
                     case "0":
                         Environment.Exit(0);
+                        break;
+                    case "1":
+                        Console.WriteLine("Please enter your age: ");
+                        int input2 = 0;
+                        int.TryParse(Console.ReadLine(), out input2);
+                        if(input2 > 64)
+                        {
+                            Console.WriteLine("Price ticket: Pensioner SEK 90");
+                        }
+                        else if(input2 < 20)
+                        {
+                            Console.WriteLine("Price ticket: Student SEK 80");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Price ticket: Ordinary SEK 120");
+                        }
+
                         break;
                     default:
                         Console.WriteLine("Invalid input");
