24b497427f87530a529d568eb234ba0d9e638448
Eugenia <musat.eugenia@gmail.com>
2024-03-05 20:35:47 +0100

    Add a new class to be able to structure data

---
+++ CinemaMainMenu/PurchaseSummary.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaMainMenu
+{
+    public class PurchaseSummary
+    {
+        public int NumberOfPersons { get; set; }
+        public int Quantity { get; set; }
+        public int PricePerTicket { get; set; }
+
+        public int TotalPrice => Quantity * PricePerTicket;
+
+    }
+}
